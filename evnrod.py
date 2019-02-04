34f981d1c3223c80c25243d52c5cfeeb5165a795
Rubycyr <46370533+Rubycyr@users.noreply.github.com>
2019-02-04 09:31:33 +0530

    Rename vowel.py to vwlsndcnsts.py

--- evnrod.py
+++
@@ -1,5 +0,0 @@
-doni=int(raw_input())
-if (doni%2)==0:
-	print "Even"
-	else:
-		print "odd"
---
+++ vwlsndcnsts.py
@@ -0,0 +1,5 @@
+roop=raw_input()
+if(roop=='a' or roop=='A' or roop=='e' or roop=='E' or roop=='i' or roop=='I' or roop=='o' or roop=='O' or roop=='u' or roop=='U'):
+	print "Vowel"
+else:
+		print "Consonant"
